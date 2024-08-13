595ca63ce61af86a968b4a9bcb316d8374cf6aa2
karkisav <saurav.karki2@icloud.com>
2024-08-13 21:50:03 +0530

    numbers.v2

--- cs50p/weeks/week3/lecture/numbers.py
+++ cs50p/weeks/week3/lecture/numbers.py
@@ -1,6 +1,9 @@
-try:
-    x = input(input("Type your number"))
-except ValueError:
-    print("x is NOT an Int please put an Int (Integer)")
-else:
-    print(f"x is {x}")
+while True:
+    try:
+        x = input(input("Type your number"))
+    except ValueError:
+        print("x is NOT an Int please put an Int (Integer)")
+    else:
+        break
+    
+print(f"x is {x}")
