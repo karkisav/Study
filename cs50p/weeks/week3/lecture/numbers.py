922d21247725eec39f6061dd83dd903ce7bd1e52
karkisav <saurav.karki2@icloud.com>
2024-08-13 21:56:52 +0530

    numbers.v4

--- cs50p/weeks/week3/lecture/numbers.py
+++ cs50p/weeks/week3/lecture/numbers.py
@@ -5,7 +5,7 @@ def main():
 def get_int():    
     while True:
         try:
-           return input(input("Type your number"))
+           return int(input("Type your number"))
         except ValueError:
             print("x is NOT an Int please put an Int (Integer)")
 
