0ca97a28676c22292fb16e1da745d53aa97d3e4c
karkisav <saurav.karki2@icloud.com>
2024-08-15 21:34:03 +0530

    Adding nameV1

--- cs50p/weeks/week4/lecture/name.py
+++ cs50p/weeks/week4/lecture/name.py
@@ -1,8 +1,7 @@
 import sys
 
-if len(sys.argv) < 2:
-    sys.exit("Too Few Arguments")
-elif len(sys.argv) > 2:
-    sys.exit("Too many Arguments")
-else:
-    print("Hello, my name is", sys.argv[1])
+if len(sys.arg) < 2:
+    sys.exit("Too few arguments")
+
+for arg in sys.argv[1:]:
+    print("Hellow my name is", arg)
