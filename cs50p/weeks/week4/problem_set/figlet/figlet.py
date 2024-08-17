253f6244d488237390ebea324d4abbbeede01816
karkisav <saurav.karki2@icloud.com>
2024-08-17 22:24:24 +0530

    Adding figlet after solving

---
+++ cs50p/weeks/week4/problem_set/figlet/figlet.py
@@ -0,0 +1,16 @@
+import pyfiglet
+import sys
+
+if len(sys.argv) == 3:
+    if sys.argv[1] != "-f" and sys.argv[1] != "--font":
+        sys.exit("Invalid Usage")   
+    
+    result = pyfiglet.figlet_format(input("Input: "), font = f"{sys.argv[2]}")
+    print(result)
+
+elif len(sys.argv) == 1:
+    result = pyfiglet.figlet_format(input("Input: "))
+    print(result)
+
+else:
+    sys.exit("Invalid Usage")
