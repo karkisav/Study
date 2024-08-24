25c2c7083d60e227ab6a2e173d88b99c98070ddd
karkisav <saurav.karki2@icloud.com>
2024-08-24 12:18:17 +0530

    students.pyV1

--- cs50p/weeks/week6/lecture/students.py
+++ cs50p/weeks/week6/lecture/students.py
@@ -1,4 +1,12 @@
+students = []
+
 with open("students.csv") as file:
     for line in file:
         name, house = line.rstrip().split(",")
-        print(f"{name} is in {house}")
+        student = {"name": name, "house": house}
+        students.append(student)
+def get_name(student):
+    return student['name']
+
+for student in sorted(students, key=get_name):
+    print(f"{student['name']} is in{student['house']}")
