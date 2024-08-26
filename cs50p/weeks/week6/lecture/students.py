36dff6098de13a1b749e417dc09092f51062c502
karkisav <saurav.karki2@icloud.com>
2024-08-26 19:03:28 +0530

    Updating the file

--- cs50p/weeks/week6/lecture/students.py
+++ cs50p/weeks/week6/lecture/students.py
@@ -3,10 +3,9 @@ import csv
 students = []
 
 with open("students.csv") as file:
-    for line in file:
-        reader = csv.DictReader(file)
-        for row in reader:
-            students.append({"name": row['name'], "house": row['house']})
+    reader = csv.DictReader(file)
+    for row in reader:
+        students.append({"name": row['name'], "house": row['house']})
             
 for student in sorted(students, key=lambda student: student["name"]):
     print(f"{student['name']} is in{student['house']}")
